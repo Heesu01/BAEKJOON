a087960e12c55c701633f69324830907abcdc1ee
Heesu01 <114339587+Heesu01@users.noreply.github.com>
2024-08-08 23:04:55 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,19 +1,12 @@
 n = int(input())
 
-if n%5==0:
-    print(n//5)
+cnt = 0 
+while n>=0:
+    if n%5==0:
+        cnt += (n//5) 
+        print(cnt)
+        break
+    n -= 3 
+    cnt += 1
 else:
-    sum=0
-    while n>0:
-        n-=3
-        sum+=1
-        if n % 5 ==0:
-            sum+= (n//5)
-            print(sum)
-            break
-        elif n==1 or n==2:
-            print(-1)
-            break
-        elif n==0:
-            print(sum)
-            break
+    print(-1)
