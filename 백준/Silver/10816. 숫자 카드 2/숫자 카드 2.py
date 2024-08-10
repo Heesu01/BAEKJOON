504e2c14bfd30b7ba8577544aacf16924606332d
Heesu01 <114339587+Heesu01@users.noreply.github.com>
2024-08-11 00:02:29 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 2884 ms, Memory: 115648 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,29 @@
+n = int(input())
+cards = list(map(int,input().split()))
+m = int(input())
+check = list(map(int,input().split()))
+
+cards.sort()
+count = {}
+
+for i in cards:
+    if i in count:
+        count[i] +=1
+    else:
+        count[i]=1
+        
+
+def binary_search(array,target,start,end):
+    if start > end:
+        return 0
+    mid = (start+end)//2
+    if array[mid] == target:
+        return count.get(target)
+    elif array[mid] > target:
+        return binary_search(array,target,start,mid-1)
+    else:
+        return binary_search(array,target,mid+1,end)
+            
+for i in range(m):
+    print(binary_search(cards,check[i],0,n-1), end=" ")
+        
