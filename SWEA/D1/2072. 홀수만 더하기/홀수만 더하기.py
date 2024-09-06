1d40ae98589acb3a503c6557f2cab3087df9d7fa
Heesu01 <114339587+Heesu01@users.noreply.github.com>
2024-09-06 19:04:36 +0900

    [D1] Title: 홀수만 더하기, Time: 95 ms, Memory: 45,040 KB -BaekjoonHub

---
+++ SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+for test_case in range(1, T + 1):
+
+    num = list(map(int, input().split()))
+    sum = 0
+
+    for i in num:
+        if i % 2 != 0:
+            sum += i
+            
+    print(f"#{test_case} {sum}")
