f4c78c5c6e6e69568b2b68e64d51ff92f0c64696
Heesu01 <114339587+Heesu01@users.noreply.github.com>
2024-09-27 23:33:26 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 240 ms, Memory: 55700 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,13 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 n,m = map(int,input().split())
 dict = {}
 
 for num in range(1,n+1):
-    po =input()
+    po =input().rstrip()
     dict[num] = po
     dict[po] = num
 
 for i in range(m):
-    ans=input()
+    ans=input().rstrip()
     if ans.isdigit():
         print(dict[int(ans)])
     else:
