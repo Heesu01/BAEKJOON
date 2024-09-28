89d64801766260c6816332c75f220b486491a27d
Heesu01 <114339587+Heesu01@users.noreply.github.com>
2024-09-28 23:22:04 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 188 ms, Memory: 49208 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,10 +1,13 @@
+import sys
+input = sys.stdin.readline
+
 n,m = map(int,input().split())
 dict = {}
 
 for _ in range(n):
-    url,pw = input().split()
+    url,pw = input().rstrip().split()
     dict[url]=pw
     
 for _ in range(m):
-    ans= input()
+    ans= input().rstrip()
     print(dict[ans])
