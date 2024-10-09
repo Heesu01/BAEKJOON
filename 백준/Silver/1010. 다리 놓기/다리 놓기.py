6d808d19c6617f57cb9ce3300d82041ae0a97f4d
Heesu01 <114339587+Heesu01@users.noreply.github.com>
2024-10-09 20:02:21 +0900

    [Silver V] Title: 다리 놓기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,11 +1,15 @@
-def factorial(n):
-    num=1
-    for i in range(1,n+1):
-        num *= i
-    return num
-
 T = int(input())
+dp = [[0] * 30 for _ in range(30)]
+for i in range(30):
+    for j in range(30):
+        if i == 1:
+            dp[i][j] = j
+        else:
+            if i == j:
+                dp[i][j] = 1
+            elif i < j:
+                dp[i][j] = dp[i-1][j-1] + dp[i][j-1]
+
 for _ in range(T):
-    n,m = map(int,input().split())
-    ans = factorial(m) // (factorial(n) * factorial(m-n))
-    print(ans)
+    n, m = list(map(int, input().split()))
+    print (dp[n][m])
