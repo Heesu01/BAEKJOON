e921aa4199aeb495714a47a2a5c8e80d4d43f304
Heesu01 <114339587+Heesu01@users.noreply.github.com>
2025-01-21 21:31:38 +0900

    [Gold V] Title: 회의실 배정, Time: 2804 ms, Memory: 54216 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,18 @@
+N = int(input())
+li = []
+
+for i in range(N):
+    start, end = map(int,input().split())
+    li.append((start, end))
+
+li.sort(key=lambda x : (x[1],x[0]))
+
+count = 1
+end = li[0][1]
+
+for i in range(1, N):
+    if li[i][0]>=end:
+        end = li[i][1]
+        count += 1
+
+print(count)
