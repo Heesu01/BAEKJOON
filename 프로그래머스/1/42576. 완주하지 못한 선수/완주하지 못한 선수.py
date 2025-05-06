138350b9b8563e0510a8708991237cf2421f1e6e
Heesu01 <114339587+Heesu01@users.noreply.github.com>
2025-05-06 23:57:12 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 44.77 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,18 @@
+def solution(participant, completion):
+    answer = ''
+    dic={}
+    for i in participant:
+        if i in dic:
+            dic[i]+=1
+        else:
+            dic[i]=1
+            
+    for i in completion:
+        if i in dic:
+            dic[i]-=1 
+            
+    for key,value in dic.items():
+        if value==1:
+            answer = key
+        
+    return answer
