b15ca0cacea5e085aecca7a5dd3c8edc838ba702
Heesu01 <114339587+Heesu01@users.noreply.github.com>
2025-06-12 20:16:40 +0900

    [level 0] Title: l로 만들기, Time: 8.93 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -2,7 +2,7 @@ def solution(myString):
     answer=''
     print(chr(108))
     for i in myString:
-        if ord(i) < 108:
+        if i < 'l':
             answer += 'l'
         else:
             answer += i
