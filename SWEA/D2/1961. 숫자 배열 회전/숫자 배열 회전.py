552a815fb14077cda987a63f57ed7c19130908e8
Heesu01 <114339587+Heesu01@users.noreply.github.com>
2025-07-19 19:33:47 +0900

    [D2] Title: 숫자 배열 회전, Time: 133 ms, Memory: 45,056 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,26 +1,16 @@
 T = int(input())
-for t in range(1,T+1):
+
+for test_case in range(1, T + 1):
     N = int(input())
-    li = list(list(map(int, input().split())) for _ in range(N))
+    matrix = [input().split() for _ in range(N)]
 
-    li3 = [[0] * N for _ in range(N)]
-    for i in range(N):
-        for j in range(N):
-            li3[i][j] = li[j][N - 1 - i]
+    rotate90 = list(zip(*matrix[::-1]))
+    rotate180 = list(zip(*rotate90[::-1]))
+    rotate270 = list(zip(*rotate180[::-1]))
 
-    li2 = [[0] * N for _ in range(N)]
+    print(f"#{test_case}")
     for i in range(N):
-        for j in range(N):
-            li2[i][j] = li3[j][N - 1 - i]
-
-    li1 = [[0] * N for _ in range(N)]
-    for i in range(N):
-        for j in range(N):
-            li1[i][j] = li2[j][N - 1 - i]
-
-    print(f"#{t}")
-    for i in range(N):
-        print(*li1[i], sep="", end=" ")
-        print(*li2[i], sep="", end=" ")
-        print(*li3[i], sep="", end=" ")
-        print() if t != T else None
+        row90 = ''.join(rotate90[i])
+        row180 = ''.join(rotate180[i])
+        row270 = ''.join(rotate270[i])
+        print(f"{row90} {row180} {row270}")
