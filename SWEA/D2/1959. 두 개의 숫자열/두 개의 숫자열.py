b8889357362ef20958df2cee2cd827f5452e24b9
Heesu01 <114339587+Heesu01@users.noreply.github.com>
2025-07-19 19:26:02 +0900

    [D2] Title: 두 개의 숫자열, Time: 59 ms, Memory: 52,480 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
@@ -1,23 +1,19 @@
 T = int(input())
+
 for test_case in range(1, T + 1):
-    N,M = map(int,input().split())
-    A = list(map(int,input().split()))
-    B = list(map(int,input().split()))
-    ans=0
-    
-    if N<M:
-        for i in range(M-N+1):
-            sum=0
-            for j in range(N):
-                sum += A[j] * B[j+i]
-            if ans<sum:
-                ans = sum
-    else:
-        for i in range(N-M+1):
-            sum=0
-            for j in range(M):
-                sum += A[j+i] * B[j]
-            if ans<sum:
-                ans = sum
-            
-    print(f"#{test_case} {ans}")
+    N, M = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+
+    if N > M:
+        A, B = B, A
+        N, M = M, N
+		
+    max_sum = 0
+    for i in range(M - N + 1):
+        sum = 0
+        for j in range(N):
+            sum += A[j] * B[i + j]
+        max_sum = max(max_sum, sum)
+
+    print(f"#{test_case} {max_sum}")
