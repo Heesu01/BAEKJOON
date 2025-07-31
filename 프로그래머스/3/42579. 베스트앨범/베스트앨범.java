8dccbdecfa16c3794dcdde8b8d9f6ce92162608c
Heesu01 <114339587+Heesu01@users.noreply.github.com>
2025-07-31 23:16:29 +0900

    [level 3] Title: 베스트앨범, Time: 1.23 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,33 +1,63 @@
 import java.util.*;
 
-class Solution {
+public class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        Map<String,Integer> genreMap = new HashMap<>();
-        Map<String, List<int[]>> songMap = new HashMap<>();
-        
-        for (int i =0; i<genres.length; i++){
-            genreMap.put(genres[i], genreMap.getOrDefault(genres[i],0)+ plays[i]);
-            songMap.computeIfAbsent(genres[i], k -> new ArrayList<>());
-            songMap.get(genres[i]).add(new int[]{i, plays[i]}); 
+        Map<String, Integer> genreMap = new HashMap<>();               
+        Map<String, List<int[]>> songMap = new HashMap<>();            
+
+        for (int i = 0; i < genres.length; i++) {
+            String genre = genres[i];
+            int play = plays[i];
+
+
+            if (genreMap.containsKey(genre)) {
+                genreMap.put(genre, genreMap.get(genre) + play);
+            } else {
+                genreMap.put(genre, play);
+            }
+
+
+            if (!songMap.containsKey(genre)) {
+                songMap.put(genre, new ArrayList<>());
+            }
+            songMap.get(genre).add(new int[]{i, play}); 
         }
-        
+
         List<String> sortedGenres = new ArrayList<>(genreMap.keySet());
-        sortedGenres.sort((a, b) -> genreMap.get(b) - genreMap.get(a));
-        
-        
+        Collections.sort(sortedGenres, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return genreMap.get(b) - genreMap.get(a);
+            }
+        });
+
         List<Integer> result = new ArrayList<>();
+
         for (String genre : sortedGenres) {
             List<int[]> songs = songMap.get(genre);
-            songs.sort((a, b) -> {
-                if (b[1] != a[1]) return b[1] - a[1];
-                return a[0] - b[0]; 
+
+            Collections.sort(songs, new Comparator<int[]>() {
+                @Override
+                public int compare(int[] a, int[] b) {
+                    if (b[1] != a[1]) {
+                        return b[1] - a[1]; 
+                    } else {
+                        return a[0] - b[0]; 
+                    }
+                }
             });
 
-            result.add(songs.get(0)[0]);
+            result.add(songs.get(0)[0]); 
             if (songs.size() > 1) {
-                result.add(songs.get(1)[0]);
+                result.add(songs.get(1)[0]); 
             }
         }
-        return result.stream().mapToInt(i -> i).toArray();
+
+        int[] answer = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        }
+
+        return answer;
     }
 }
