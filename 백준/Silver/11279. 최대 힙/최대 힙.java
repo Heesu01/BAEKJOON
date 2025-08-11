1701794a17289faa32a1fbe43290bfa5f55e5d40
Heesu01 <114339587+Heesu01@users.noreply.github.com>
2025-08-11 22:39:36 +0900

    [Silver II] Title: 최대 힙, Time: 276 ms, Memory: 26432 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine().trim());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                sb.append(pq.isEmpty() ? 0 : pq.poll()).append('\n');
+            } else {
+                pq.add(x);
+            }
+        }
+
+        System.out.print(sb.toString());
+    }
+}
