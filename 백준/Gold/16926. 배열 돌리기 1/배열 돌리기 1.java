2fbfae86d1f4c1907fe2fcf6c4b6cb785c968a3b
Heesu01 <114339587+Heesu01@users.noreply.github.com>
2025-08-17 21:34:06 +0900

    Update 배열 돌리기 1.java

--- 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -2,46 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int R = Integer.parseInt(st.nextToken());
 
-        int[][] A = new int[N][M];
+        int[][] arr = new int[N][M];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) A[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
 
-        int layers = Math.min(N, M) / 2;
-        for (int s = 0; s < layers; s++) {
-            int top = s, left = s;
-            int bottom = N - 1 - s, right = M - 1 - s;
-            int h = bottom - top + 1, w = right - left + 1;
-            int len = 2 * (h + w) - 4; 
-            int[] ring = new int[len];
+        for (int r = 0; r < R; r++) {
+            int layers = Math.min(N, M) / 2;
+            for (int s = 0; s < layers; s++) {
+                int top = s, left = s;
+                int bottom = N - 1 - s, right = M - 1 - s;
 
-            int idx = 0;
-            for (int c = left; c <= right; c++) ring[idx++] = A[top][c];              // 위 행 (→)
-            for (int r = top + 1; r <= bottom; r++) ring[idx++] = A[r][right];        // 오른쪽 열 (↓)
-            for (int c = right - 1; c >= left; c--) ring[idx++] = A[bottom][c];       // 아래 행 (←)
-            for (int r = bottom - 1; r > top; r--) ring[idx++] = A[r][left];          // 왼쪽 열 (↑)
+                int start = arr[top][left];
 
-            int k = R % len;
-            if (k == 0) continue;
+                for (int c = left; c < right; c++) arr[top][c] = arr[top][c + 1];
+                for (int i = top; i < bottom; i++) arr[i][right] = arr[i + 1][right];
+                for (int c = right; c > left; c--) arr[bottom][c] = arr[bottom][c - 1];
+                for (int i = bottom; i > top; i--) arr[i][left] = arr[i - 1][left];
 
-            idx = 0;
-            for (int c = left; c <= right; c++) A[top][c] = ring[(idx++ + k) % len];
-            for (int r = top + 1; r <= bottom; r++) A[r][right] = ring[(idx++ + k) % len];
-            for (int c = right - 1; c >= left; c--) A[bottom][c] = ring[(idx++ + k) % len];
-            for (int r = bottom - 1; r > top; r--) A[r][left] = ring[(idx++ + k) % len];
+                arr[top + 1][left] = start;
+            }
         }
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) sb.append(A[i][j]).append(' ');
+            for (int j = 0; j < M; j++) sb.append(arr[i][j]).append(' ');
             sb.append('\n');
         }
         System.out.print(sb.toString());
