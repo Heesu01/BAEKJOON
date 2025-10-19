b5d521b681c72f6fccf63fe949a1f0530f66d2ba
Heesu01 <114339587+Heesu01@users.noreply.github.com>
2025-10-19 22:23:56 +0900

    [Silver III] Title: N과 M (1), Time: 228 ms, Memory: 22496 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+    static int N,M,arr[];
+    static boolean visit[];
+    static StringBuilder sb;
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            sb = new StringBuilder();
+      
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            arr = new int[M];
+            visit = new boolean[N];
+            
+            perm(0);
+            System.out.println(sb);
+        
+    }
+    public static void perm(int depth){
+        
+        if(depth==M){
+            for(int i:arr){
+                sb.append(i).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+        
+        for(int i = 0; i<N; i++){
+            if(!visit[i]){
+                visit[i]=true;
+                arr[depth]=i+1;
+                perm(depth+1);
+                visit[i]=false;
+            }
+        }
+    }                                        
+}
