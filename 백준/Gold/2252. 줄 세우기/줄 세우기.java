5376dc10a4a20bc89d83cc53af20750bd5e68f0a
Heesu01 <114339587+Heesu01@users.noreply.github.com>
2025-11-03 14:10:07 +0900

    [Gold III] Title: 줄 세우기, Time: 396 ms, Memory: 45616 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -2,42 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        List<Integer>[] graph = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
-
-        int[] indegree = new int[N + 1];
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            graph[A].add(B);
-            indegree[B]++;
-        }
-
-        Queue<Integer> q = new ArrayDeque<>();
-        for (int i = 1; i <= N; i++) {
-            if (indegree[i] == 0) q.offer(i);
-        }
-
-        StringBuilder sb = new StringBuilder();
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-            sb.append(cur).append(' ');
-
-            for (int nxt : graph[cur]) {
-                indegree[nxt]--;
-                if (indegree[nxt] == 0) q.offer(nxt);
-            }
-        }
-
-        System.out.println(sb);
+  public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		List<Integer>[] g = new ArrayList[N+1];
+		for(int i=1;i<=N;i++) g[i]= new ArrayList<>();
+		
+		int[] indegree = new int[N+1];
+		
+		for(int i=0;i<M;i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			g[A].add(B);
+			indegree[B]++;
+		}
+		
+		Queue<Integer> q = new ArrayDeque<>();
+		for(int i=1;i<=N;i++) {
+			if(indegree[i]==0) q.add(i);
+		}
+		
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+			sb.append(cur).append(" ");
+			
+			for(int nxt: g[cur]) {
+				indegree[nxt]--;
+				if(indegree[nxt]==0) q.add(nxt);
+			}
+		}
+		System.out.println(sb);
     }
 }
