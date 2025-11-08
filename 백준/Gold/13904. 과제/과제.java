9b8c6fe655634d1faab42c6dcd85f6dba39cf412
Heesu01 <114339587+Heesu01@users.noreply.github.com>
2025-11-08 19:55:57 +0900

    [Gold III] Title: 과제, Time: 120 ms, Memory: 14728 KB -BaekjoonHub

--- 백준/Gold/13904. 과제/과제.java
+++ 백준/Gold/13904. 과제/과제.java
@@ -6,33 +6,28 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine().trim());
 
-        int[][] arr = new int[N][2];
+        int[][] task = new int[N][2]; 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int d = Integer.parseInt(st.nextToken());
-            int r = Integer.parseInt(st.nextToken());
-            arr[i][0] = d; arr[i][1] = r;
+            int w = Integer.parseInt(st.nextToken());
+            task[i][0] = d;
+            task[i][1] = w;
         }
 
-        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+        Arrays.sort(task, (a, b) -> a[0] - b[0]); 
 
         PriorityQueue<Integer> minHeap = new PriorityQueue<>(); 
-        
-        for (int[] job : arr) {
-            int deadline = job[0];
-            int reward   = job[1];
-
-            minHeap.offer(reward);
-            if (minHeap.size() > deadline)
+        for (int[] t : task) {
+		        
+            minHeap.offer(t[1]);         
+            if (minHeap.size() > t[0]) {  
                 minHeap.poll();
+            }
         }
 
-        long ans = 0;
-        
-        while (!minHeap.isEmpty()){
-            ans += minHeap.poll();
-        }
-        
-        System.out.println(ans);
+        long sum = 0;
+        while (!minHeap.isEmpty()) sum += minHeap.poll();
+        System.out.println(sum);
     }
 }
