7d16048bf7ed333fb1eecca69ebe74a35be2f987
Heesu01 <114339587+Heesu01@users.noreply.github.com>
2025-12-15 00:39:22 +0900

    [level 3] Title: 합승 택시 요금, Time: 34.08 ms, Memory: 68.8 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,66 +1,62 @@
 import java.util.*;
 
 class Solution {
-    static class Edge {
-        int to;
-        int w;
-        Edge(int t, int w) {
-            this.to = t;
-            this.w = w;
-        }
-    }
 
     static final long INF = Long.MAX_VALUE / 4;
 
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        // 인접 리스트
-        List<Edge>[] g = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) g[i] = new ArrayList<>();
+
+        List<int[]>[] graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
 
         for (int[] f : fares) {
-            int u = f[0], v = f[1], w = f[2];
-            g[u].add(new Edge(v, w));
-            g[v].add(new Edge(u, w));
+            int u = f[0], v = f[1], cost = f[2];
+            graph[u].add(new int[]{v, cost});
+            graph[v].add(new int[]{u, cost});
         }
 
-        long[] distS = dijkstra(n, s, g);
-        long[] distA = dijkstra(n, a, g);
-        long[] distB = dijkstra(n, b, g);
+        long[] distS = dijkstra(n, s, graph);
+        long[] distA = dijkstra(n, a, graph);
+        long[] distB = dijkstra(n, b, graph);
 
-        long ans = INF;
+        long answer = INF;
         for (int k = 1; k <= n; k++) {
-            if (distS[k] == INF || distA[k] == INF || distB[k] == INF) continue;
             long cost = distS[k] + distA[k] + distB[k];
-            if (cost < ans) ans = cost;
+            if (cost < answer) answer = cost;
         }
 
-        return (int) ans;
+        return (int) answer;
     }
 
-    private long[] dijkstra(int n, int start, List<Edge>[] g) {
+    private long[] dijkstra(int n, int start, List<int[]>[] graph) {
+
         long[] dist = new long[n + 1];
         Arrays.fill(dist, INF);
         dist[start] = 0;
 
-        PriorityQueue<long[]> pq = new PriorityQueue<>(Comparator.comparingLong(o -> o[1]));
+        PriorityQueue<long[]> pq =
+                new PriorityQueue<>(Comparator.comparingLong(o -> o[1]));
         pq.add(new long[]{start, 0});
 
         while (!pq.isEmpty()) {
             long[] cur = pq.poll();
-            int x = (int) cur[0];
-            long d = cur[1];
+            int now = (int) cur[0];
+            long nowCost = cur[1];
 
-            if (d > dist[x]) continue;
+            if (nowCost > dist[now]) continue;
 
-            for (Edge e : g[x]) {
-                int nx = e.to;
-                long nd = d + e.w;
-                if (nd < dist[nx]) {
-                    dist[nx] = nd;
-                    pq.add(new long[]{nx, nd});
+            for (int[] edge : graph[now]) {
+                int next = edge[0];
+                int w = edge[1];
+                long newCost = nowCost + w;
+
+                if (newCost < dist[next]) {
+                    dist[next] = newCost;
+                    pq.add(new long[]{next, newCost});
                 }
             }
         }
+
         return dist;
     }
 }
